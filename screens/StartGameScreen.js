d8463c7c80d75768a2f7ce993d3871139dccbbc2
leegyuseung <lgs4002@naver.com>
2022-09-05 21:54:14 +0900

    컴포넌트 생성

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,13 @@
+import { Button, TextInput } from "react-native";
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      <Button />
+      <Button />
+    </View>
+  );
+}
+
+export default StartGameScreen;
